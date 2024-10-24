7c2a598dc9515d2014a5fdbffae5988a11c64844
Herman Diaz Luna <herosdial@hotmail.com>
2024-10-24 17:30:39 -0500

    add dtos

---
+++ src/main/java/com/project/base/proyectobase/infrastructure/entry_point/empleado/dto/EmpleadoResponseDTO.java
@@ -0,0 +1,17 @@
+package com.project.base.proyectobase.infrastructure.entry_point.empleado.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmpleadoResponseDTO {
+
+    private Integer id;
+    private String cedula;
+    private String nombre;
+}
